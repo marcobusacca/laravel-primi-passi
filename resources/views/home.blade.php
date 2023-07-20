66f20284ef318fbbe5a6f22fb184a0938977953e
marcobusacca <marcobusacca01@gmail.com>
2023-07-20 16:38:26 +0200

    [FEAT] home page bootstrap cdn css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
+        <!-- BootStrap CDN CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
         <!-- Styles -->
         <style>
             body {
